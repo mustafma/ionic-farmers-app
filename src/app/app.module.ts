d0702bda33678dc518dc7df39e31126d9f0dfe30
Mustafa Adam <mustafa.adam@blueback-reservoir.com>
2018-05-11 05:05:09 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+//*********** ionic Native **************/
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { MyApp } from './app.component';
+
+//***********  Angularfire2 v4 **************/
+
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+//*********** Provider **************/
+import { AuthData } from '../providers/auth-data';
+import { RadioPlayer } from '../providers/radio-service';
+
+
+//*********** Image Gallery **************/
+import { GalleryModal } from 'ionic-gallery-modal';
+import { ZoomableImage } from 'ionic-gallery-modal';
+
+
+//********** firebase configuration  ************ */
+  var config = {
+    apiKey: "AIzaSyAK73eiucib4vX5hyEXeXCKEPp4_2nj8Eg",
+    authDomain: "findlost-4ca6b.firebaseapp.com",
+    databaseURL: "https://findlost-4ca6b.firebaseio.com",
+    projectId: "findlost-4ca6b",
+    storageBucket: "findlost-4ca6b.appspot.com",
+    messagingSenderId: "884476581525"
+  };
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({
+      name: '__mydb',
+         driverOrder: ['sqlite', 'indexeddb', 'websql']
+    }),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthData
+  ]
+})
+export class AppModule {}
